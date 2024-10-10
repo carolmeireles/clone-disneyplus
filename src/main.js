8ee3db9ba8867fbbb01337b33112eba40f3400fa
Carol <email@exemplo.com>
2024-10-10 13:21:56 -0300

    ajustes no mobile

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]');
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
 
+    window.addEventListener('scroll', function() {
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            ocultaElementosHeader();
+        } else {
+            exibeElementosHeader();
+        }
+    });
+    
+    // Seção de atrações - Programação das abas
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
             const abaAlvo = botao.target.dataset.tabButton;
@@ -13,15 +26,20 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }
 
+    //Seção FAQ - Accordion
     for (let i=0; i < questions.length; i++) {
         questions[i].addEventListener('click', toggleAnswer);
     }
 });
 
-function toggleAnswer(elemento) {
-    const classe = 'faq__questions__item--is-open';
-    const elementoPai = elemento.target.parentNode;
-    elementoPai.classList.toggle(classe);
+function ocultaElementosHeader() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function exibeElementosHeader() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
 }
 
 function removeBotaoAtivo() {
@@ -38,4 +56,10 @@ function escondeTodasAbas() {
     for (let i = 0; i < tabsContainer.length; i++) {
         tabsContainer[i].classList.remove('shows__list--is-active');
     }
+}
+
+function toggleAnswer(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+    elementoPai.classList.toggle(classe);
 }
